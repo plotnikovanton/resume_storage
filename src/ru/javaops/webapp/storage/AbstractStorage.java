1be217615772d5ef3172dc1f8c364a590c4a2aae
Anton Plotnikov <plotnikovanton.evg@gmail.com>
2019-12-03 22:59:53 +0300

    Adding logging

--- src/ru/javaops/webapp/storage/AbstractStorage.java
+++ src/ru/javaops/webapp/storage/AbstractStorage.java
@@ -6,8 +6,11 @@ import ru.javaops.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     protected abstract SK getSearchKey(String key);
 
@@ -25,26 +28,31 @@ public abstract class AbstractStorage<SK> implements Storage {
 
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK key = getExistedKey(uuid);
         return doGet(key);
     }
 
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         SK key = getNotExistedKey(resume.getUuid());
         doSave(key, resume);
     }
 
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         SK key = getExistedKey(resume.getUuid());
         doUpdate(key, resume);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK key = getExistedKey(uuid);
         doDelete(key);
     }
 
     public List<Resume> getAllSorted() {
+        LOG.info("GetAllSorted");
         List<Resume> resumes = doGetAllSorted();
         Collections.sort(resumes);
         return resumes;
@@ -53,6 +61,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getExistedKey(String uuid) {
         SK key = getSearchKey(uuid);
         if (!isExist(key)) {
+            LOG.warning("Resume " + uuid + " doesn't exist");
             throw new NotExistStorageException(uuid);
         }
         return key;
@@ -61,6 +70,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getNotExistedKey(String uuid) {
         SK key = getSearchKey(uuid);
         if (isExist(key)) {
+            LOG.warning("Resume " + uuid + " already exists");
             throw new ExistStorageException(uuid);
         }
         return key;
