8b57c008cfc2f41ff55db0d4acb806f817e8b985
Anton Plotnikov <plotnikovanton.evg@gmail.com>
2020-04-23 00:33:09 +0300

    Adding JsonStreamSerializer, JsonParser

---
+++ src/ru/javaops/webapp/storage/serializer/JsonStreamSerializer.java
@@ -0,0 +1,24 @@
+package ru.javaops.webapp.storage.serializer;
+
+import ru.javaops.webapp.model.Resume;
+import ru.javaops.webapp.util.JsonParser;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class JsonStreamSerializer implements StreamSerializer {
+
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try (Reader r = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            return JsonParser.read(r, Resume.class);
+        }
+    }
+
+    @Override
+    public void doWrite(OutputStream os, Resume resume) throws IOException {
+        try (Writer w = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+            JsonParser.write(resume, w);
+        }
+    }
+}
